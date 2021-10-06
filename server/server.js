8b027fcf1aedaf1e3d73e6e1e7a2e93b4b8501a0
William Crownover <williamcrownover1@gmail.com>
2021-10-05 21:46:42 -0700

    Setup server.js to use apollo server

--- server/server.js
+++ server/server.js
@@ -2,11 +2,26 @@ const express = require('express');
 // Adding apollo server
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+// Adding schema files and authMiddleware
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
+
+// THIS WILL BE REMOVED!
 const routes = require('./routes');
+// THIS WILL BE REMOVED!
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Define the server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Adding the applyMiddleware line
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +31,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// THIS WILL BE REMOVED!
 app.use(routes);
+// THIS WILL BE REMOVED!
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
